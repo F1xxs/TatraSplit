9594067431c00db59446f72e6322190ad41de286
F1xx <mishaf1x.ol@gmail.com>
2026-04-18 20:05:15 +0200

    disable mockup

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,15 +1,22 @@
 import axios from 'axios'
-import { handleMock } from '@/lib/mock'
 
 export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8000/api/v1'
-const useMock = import.meta.env.VITE_USE_MOCK !== 'false'
 
-const axiosApi = axios.create({
+function getUserHandle() {
+  return localStorage.getItem('userHandle') || '@misha'
+}
+
+export const api = axios.create({
   baseURL: API_BASE,
   timeout: 10000,
 })
 
-axiosApi.interceptors.response.use(
+api.interceptors.request.use((config) => {
+  config.headers['X-User-Handle'] = getUserHandle()
+  return config
+})
+
+api.interceptors.response.use(
   (res) => res,
   (err) => {
     const msg =
@@ -20,17 +27,3 @@ axiosApi.interceptors.response.use(
     return Promise.reject(new Error(msg))
   },
 )
-
-async function runMock(method, url, data) {
-  const result = await handleMock(method, url, data)
-  return { data: result }
-}
-
-export const api = {
-  get(url, config) {
-    return useMock ? runMock('GET', url) : axiosApi.get(url, config)
-  },
-  post(url, data, config) {
-    return useMock ? runMock('POST', url, data) : axiosApi.post(url, data, config)
-  },
-}
